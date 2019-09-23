af43be402e547e469bbc2b139b507a68a7017309
Thethela Faltein <thethelafaltein@gmail.com>
2019-09-23 14:50:45 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
+
+
+
 public class Main
 {
+
+    public static void main(String args[]) throws Exception
+    {
+
+        Symbol_Table_Simulator sm = new Symbol_Table_Simulator(args[0]);
+
+        System.out.println(sm.process());
+
+
+
+
+
+
+    }
+
 }
